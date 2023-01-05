7d96d95b13e53edf3f9570e762a123324444fc45
surajlifeean <surajjeswara@yahoo.com>
2023-01-06 01:15:54 +0530

    send OTP gateway complete

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+   
+namespace App\Http\Controllers\API;   
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+use App\Models\User;
+use App\Models\otp;
+
+   
+class AuthController extends BaseController
+{
+
+    public function signin(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            $authUser = Auth::user(); 
+            $success['token'] =  $authUser->createToken('MyAuthApp')->plainTextToken; 
+            $success['name'] =  $authUser->name;
+   
+            return $this->sendResponse($success, 'User signed in');
+        } 
+        else{ 
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        } 
+    }
+
+    public function signup(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Error validation', $validator->errors());       
+        }
+   
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyAuthApp')->plainTextToken;
+        $success['name'] =  $user->name;
+   
+        return $this->sendResponse($success, 'User created successfully.');
+    }
+
+    public function sendotp(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'mobile' => 'required',
+            'role' =>   'required',
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Error validation', $validator->errors());       
+        }
+   
+        $input = $request->all();
+        $input['otp'] = 1000;  // replace this with SMS gateway
+        // return $input;
+        $user = otp::create($input);
+        $success['otp'] =  $user->otp;
+   
+        return $this->sendResponse($success, 'OTP sent successfully.');
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+  
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+  
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\BaseController;
+
+
+  
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+  
+Route::post('login', [AuthController::class, 'signin']);
+Route::post('sendotp', [AuthController::class, 'sendotp']);
+Route::post('register', [AuthController::class, 'signup']);
+     
+Route::middleware('auth:sanctum')->group( function () {
+    // Route::resource('blogs', BlogController::class);
+});
