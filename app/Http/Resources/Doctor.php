b4fc58edc64014463c38a65a6b4eb6b20f51ffcd
surajlifeean <surajjeswara@yahoo.com>
2023-02-06 23:56:15 +0530

    path of prescription added

--- app/Http/Resources/Doctor.php
+++ app/Http/Resources/Doctor.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Lab extends JsonResource
+class Doctor extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -16,14 +16,16 @@ class Lab extends JsonResource
     {
         return [
             'id' => $this->id,
-            'test_name' => $this->test_name,
-            'payment_mode' => $this->payment_mode,
-            'date_of_test' => $this->date_of_test,
-            'prescription_exists_flag' => $this->prescription_exists_flag,
-            'prescription' => "assets/labtestsuploads/prescription/".$this->prescription,
-            'user_id' => $this->user_id,
-            'address' => $this->address,
-            'collect_sample_from_home_flag' => $this ->collect_sample_from_home_flag,
+            'name' => $this->name,
+            'phone' => $this->phone,
+            'email' => $this->email,
+            'language' => $this->language,
+            'yearsOfExperience' => $this->yearsOfExperience,
+            'education' => $this->education,
+            'fees' => $this->fees,
+            'doctor_registration_no' => $this->doctor_registration_no,
+            'treatment_type' => $this->id,
+            'image' => $this->image,
             'created_at' => $this->created_at->format('m/d/Y'),
             'updated_at' => $this->updated_at->format('m/d/Y'),
         ];
