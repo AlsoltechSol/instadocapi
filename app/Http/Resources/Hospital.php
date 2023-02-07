21ac073c79b369d7cff4d340f255a57a6aab2bf7
Sandip Shaw <sandip.alsoltech.astw001@gmail.com>
2023-02-07 17:16:49 +0530

    api-in-hospital-specialization-city

---
+++ app/Http/Resources/Hospital.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Hospital extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        //return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'location' => $this->location,
+            'city' => $this->city,
+            'country' => $this->country,           
+            'created_at' => $this->created_at->format('m/d/Y'),
+            'updated_at' => $this->updated_at->format('m/d/Y'),
+        ];
+    }
+}
--- app/Models/Hospital.php
+++ app/Models/Hospital.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Hospital extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'location',
+        'city',
+        'country',     
+    ];
 }
